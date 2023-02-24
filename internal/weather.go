d2e579e3ae9c4e813b65dbe6147573a5eacf6bd9
Facundo Lerena <faculerena@gmail.com>
2023-02-24 17:12:46 -0300

    updated weather, now can ask for /current [city] or /nextdays [city]

--- internal/weather.go
+++ internal/weather.go
@@ -10,6 +10,95 @@ import (
 	"time"
 )
 
+type WeatherDataForecast struct {
+	Location struct {
+		Name           string  `json:"name"`
+		Region         string  `json:"region"`
+		Country        string  `json:"country"`
+		Lat            float64 `json:"lat"`
+		Lon            float64 `json:"lon"`
+		TzId           string  `json:"tz_id"`
+		LocaltimeEpoch int     `json:"localtime_epoch"`
+		Localtime      string  `json:"localtime"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		IsDay     int     `json:"is_day"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+		WindKph    float64 `json:"wind_kph"`
+		WindDegree int     `json:"wind_degree"`
+		WindDir    string  `json:"wind_dir"`
+		PressureMb float64 `json:"pressure_mb"`
+		PrecipMm   float64 `json:"precip_mm"`
+		Humidity   int     `json:"humidity"`
+		Cloud      int     `json:"cloud"`
+		FeelslikeC float64 `json:"feelslike_c"`
+		VisKm      float64 `json:"vis_km"`
+		Uv         float64 `json:"uv"`
+		GustKph    float64 `json:"gust_kph"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Date      string `json:"date"`
+			DateEpoch int    `json:"date_epoch"`
+			Day       struct {
+				MaxtempC          float64 `json:"maxtemp_c"`
+				MintempC          float64 `json:"mintemp_c"`
+				AvgtempC          float64 `json:"avgtemp_c"`
+				MaxwindKph        float64 `json:"maxwind_kph"`
+				TotalprecipMm     float64 `json:"totalprecip_mm"`
+				TotalsnowCm       float64 `json:"totalsnow_cm"`
+				AvgvisKm          float64 `json:"avgvis_km"`
+				Avghumidity       float64 `json:"avghumidity"`
+				DailyWillItRain   int     `json:"daily_will_it_rain"`
+				DailyChanceOfRain int     `json:"daily_chance_of_rain"`
+				DailyWillItSnow   int     `json:"daily_will_it_snow"`
+				DailyChanceOfSnow int     `json:"daily_chance_of_snow"`
+				Condition         struct {
+				} `json:"condition"`
+				Uv float64 `json:"uv"`
+			} `json:"day"`
+			Astro struct {
+				Sunrise          string `json:"sunrise"`
+				Sunset           string `json:"sunset"`
+				Moonrise         string `json:"moonrise"`
+				Moonset          string `json:"moonset"`
+				MoonPhase        string `json:"moon_phase"`
+				MoonIllumination string `json:"moon_illumination"`
+				IsMoonUp         int    `json:"is_moon_up"`
+				IsSunUp          int    `json:"is_sun_up"`
+			} `json:"astro"`
+			Hour []struct {
+				TimeEpoch int     `json:"time_epoch"`
+				Time      string  `json:"time"`
+				TempC     float64 `json:"temp_c"`
+				IsDay     int     `json:"is_day"`
+				Condition struct {
+				} `json:"condition"`
+				WindKph      float64 `json:"wind_kph"`
+				WindDegree   int     `json:"wind_degree"`
+				WindDir      string  `json:"wind_dir"`
+				PressureMb   float64 `json:"pressure_mb"`
+				PrecipMm     float64 `json:"precip_mm"`
+				Humidity     int     `json:"humidity"`
+				Cloud        int     `json:"cloud"`
+				FeelslikeC   float64 `json:"feelslike_c"`
+				WindchillC   float64 `json:"windchill_c"`
+				HeatindexC   float64 `json:"heatindex_c"`
+				DewpointC    float64 `json:"dewpoint_c"`
+				WillItRain   int     `json:"will_it_rain"`
+				ChanceOfRain int     `json:"chance_of_rain"`
+				WillItSnow   int     `json:"will_it_snow"`
+				VisKm        float64 `json:"vis_km"`
+				GustKph      float64 `json:"gust_kph"`
+				Uv           float64 `json:"uv"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
 type WeatherDataCurrent struct {
 	Location struct {
 		Name           string  `json:"name"`
@@ -41,17 +130,26 @@ type WeatherDataCurrent struct {
 	} `json:"current"`
 }
 
-func makeUrl(key string, gps string) (url string) {
+func makeUrl(key string, gps string, what string) (url string) {
 
-	url = fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + gps + "&aqi=no")
-	return url
+	switch what {
+	case "current":
+		url = fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + gps + "&aqi=no")
+		return url
+
+	case "forecast":
+		//http://api.weatherapi.com/v1/forecast.json?key= 579a11c94dc946fb84a230435230101&q=mar de ajo&days=1&aqi=no&alerts=no
+		url = fmt.Sprintf("https://api.weatherapi.com/v1/forecast.json?key=" + key + "&q=" + gps + "&days=3" + "&aqi=no")
+		return url
+	}
+	return ""
 }
 
 func GetCurrent(gps string) (WeatherDataCurrent, error) {
 	var weatherData WeatherDataCurrent
 	weatherKey := os.Getenv("API_KEY")
 
-	response, err := http.Get(makeUrl(weatherKey, gps))
+	response, err := http.Get(makeUrl(weatherKey, gps, "current"))
 	if err != nil {
 		log.Println("Error in response:", err)
 		return weatherData, err
@@ -80,6 +178,59 @@ func Current(update tgbotapi.Update, gps string) tgbotapi.MessageConfig {
 	formattedTime := t.Format("15:04")
 
 	msgContent := fmt.Sprintf("Hora: %s\nTemperatura actual en %s: %v°C\nHumedad: %v%%", formattedTime, weatherData.Location.Name, weatherData.Current.TempC, weatherData.Current.Humidity)
+	return tgbotapi.NewMessage(update.Message.Chat.ID, msgContent)
+
+}
+
+func GetForecast(gps string) (WeatherDataForecast, error) {
+
+	var weatherData WeatherDataForecast
+	weatherKey := os.Getenv("API_KEY")
+
+	response, err := http.Get(makeUrl(weatherKey, gps, "forecast"))
+	if err != nil {
+		log.Println("Error in response:", err)
+		return weatherData, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&weatherData)
+	if err != nil {
+		log.Println("Error: decoding", err)
+		return weatherData, err
+	}
+
+	return weatherData, nil
+}
+
+func Forecast(update tgbotapi.Update, gps string) tgbotapi.MessageConfig {
+	weatherData, err := GetForecast(gps)
+	if err != nil {
+		log.Println(err)
+	}
+	/*
+
+		t, err := time.Parse("2006-01-02 15:04", weatherData.Location.Localtime)
+		if err != nil {
+			log.Println(err)
+		}
+		formattedTime := t.Format("02/01")
+
+	*/
+
+	var allDaysForecast []string
+
+	for i := 0; i <= 2; i++ {
+		min := weatherData.Forecast.Forecastday[i].Day.MintempC
+		max := weatherData.Forecast.Forecastday[i].Day.MaxtempC
+		h := weatherData.Forecast.Forecastday[i].Day.Avghumidity
+		rain := weatherData.Forecast.Forecastday[i].Day.DailyChanceOfRain
+
+		ms := fmt.Sprintf("    -Max: %v°C\n    -Min: %v°C\n    -Humedad: %v%%\n    -Precipitaciones: %v%%\n", max, min, h, rain)
+		allDaysForecast = append(allDaysForecast, ms)
+	}
+
+	msgContent := fmt.Sprintf("> Mañana:\n%v> Pasado Mañana:\n%v>PasadoPasado Mañana:\n%v", allDaysForecast[0], allDaysForecast[1], allDaysForecast[2])
 	return tgbotapi.NewMessage(update.Message.Chat.ID, msgContent)
 
 }
--- main.go
+++ main.go
@@ -17,11 +17,18 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	updates, err := bot.GetUpdatesChan(u)
+
+	verification := tgbotapi.NewMessage(1714711619, "Bot is up.")
+	_, err = bot.Send(verification)
+	if err != nil {
+		log.Println(err)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
@@ -31,13 +38,26 @@ func main() {
 		if update.Message.IsCommand() { // listen to command messages only
 			switch update.Message.Command() {
 			case "current":
+				userInput := update.Message.CommandArguments()
 
-				_, err = bot.Send(internal.Current(update))
-				if err != nil {
-					log.Println(err)
+				if userInput == "" {
+
+					_, err = bot.Send(internal.Current(update, "Buenos_Aires"))
+					if err != nil {
+						log.Println(err)
+					}
+				} else {
+
+					_, err = bot.Send(internal.Current(update, strings.ReplaceAll(userInput, " ", "-")))
+					if err != nil {
+						log.Println(err)
+					}
 				}
 			case "ping":
-				msgContent := fmt.Sprintf("I'm alive, server time is %v", time.Now())
+
+				t := time.Now().Format("02/04/06, 15:04:05")
+
+				msgContent := fmt.Sprintf("I'm alive, server time is %v", t)
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgContent)
 
 				_, err = bot.Send(msg)
@@ -56,8 +76,15 @@ func main() {
 					}
 
 				}
-				internal.Handshake(args[0], args[1], args[2])
-				path := "./output/output.jpg"
+				err := internal.Handshake(args[0], args[1], args[2])
+				if err != nil {
+					message := tgbotapi.NewMessage(update.Message.Chat.ID, "error buscando la imagen, seguro no se subio al servidor")
+					_, err = bot.Send(message)
+					if err != nil {
+						log.Println(err)
+					}
+				}
+				path := "/assets/output.jpg"
 				file, err := os.Open(path)
 				if err != nil {
 					log.Println(err)
@@ -77,8 +104,38 @@ func main() {
 				if err != nil {
 					log.Println(err)
 				}
-			}
+			case "createfile":
 
+				file, err := os.Create("/assets/example.txt")
+				if err != nil {
+					log.Println(err)
+				}
+				defer file.Close()
+
+				_, err = file.WriteString("Hello, world!\n")
+				if err != nil {
+					log.Println(err)
+				}
+
+				fmt.Println("File created successfully!")
+			case "nextdays":
+				userInput := update.Message.CommandArguments()
+
+				if userInput == "" {
+
+					_, err = bot.Send(internal.Forecast(update, "Buenos_Aires"))
+					if err != nil {
+						log.Println(err)
+					}
+				} else {
+
+					_, err = bot.Send(internal.Forecast(update, strings.ReplaceAll(userInput, " ", "&20")))
+					if err != nil {
+						log.Println(err)
+					}
+				}
+
+			}
 		}
 
 		//log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
